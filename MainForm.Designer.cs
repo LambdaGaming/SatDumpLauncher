9fb353cf044e894ca8e93518c7df48f079bdb4cd
LambdaGaming <starwars18m@gmail.com>
2024-02-17 15:08:17 -0500

    Update MainForm.Designer.cs

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -38,19 +38,20 @@
 			// 
 			// LaunchButton
 			// 
-			LaunchButton.BackColor = Color.FromArgb(         68  ,         68  ,         68   );
+			LaunchButton.BackColor = Color.FromArgb(       68,       68,       68 );
 			LaunchButton.Dock = DockStyle.Bottom;
 			LaunchButton.FlatAppearance.BorderSize = 0;
 			LaunchButton.FlatStyle = FlatStyle.Flat;
-			LaunchButton.Font = new Font( "Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point );
+			LaunchButton.Font = new Font( "Segoe UI", 16F );
 			LaunchButton.ForeColor = Color.White;
-			LaunchButton.Location = new Point( 0, 130 );
+			LaunchButton.Location = new Point( 0, 97 );
+			LaunchButton.Margin = new Padding( 3, 2, 3, 2 );
 			LaunchButton.Name = "LaunchButton";
-			LaunchButton.Size = new Size( 343, 52 );
+			LaunchButton.Size = new Size( 300, 39 );
 			LaunchButton.TabIndex = 0;
 			LaunchButton.Text = "LAUNCH";
 			LaunchButton.UseVisualStyleBackColor = false;
-			LaunchButton.Click += LaunchButton_Click;
+			LaunchButton.Click +=  LaunchButton_Click ;
 			// 
 			// NightlyCheck
 			// 
@@ -59,9 +60,10 @@
 			NightlyCheck.CheckState = CheckState.Checked;
 			NightlyCheck.FlatStyle = FlatStyle.Flat;
 			NightlyCheck.ForeColor = Color.White;
-			NightlyCheck.Location = new Point( 12, 42 );
+			NightlyCheck.Location = new Point( 10, 32 );
+			NightlyCheck.Margin = new Padding( 3, 2, 3, 2 );
 			NightlyCheck.Name = "NightlyCheck";
-			NightlyCheck.Size = new Size( 147, 24 );
+			NightlyCheck.Size = new Size( 119, 19 );
 			NightlyCheck.TabIndex = 1;
 			NightlyCheck.Text = "Use Nightly Builds";
 			NightlyCheck.UseVisualStyleBackColor = true;
@@ -71,9 +73,9 @@
 			StatLabel.AutoSize = true;
 			StatLabel.Dock = DockStyle.Bottom;
 			StatLabel.ForeColor = Color.White;
-			StatLabel.Location = new Point( 0, 110 );
+			StatLabel.Location = new Point( 0, 82 );
 			StatLabel.Name = "StatLabel";
-			StatLabel.Size = new Size( 35, 20 );
+			StatLabel.Size = new Size( 27, 15 );
 			StatLabel.TabIndex = 2;
 			StatLabel.Text = "Test";
 			// 
@@ -82,9 +84,10 @@
 			CleanCheck.AutoSize = true;
 			CleanCheck.FlatStyle = FlatStyle.Flat;
 			CleanCheck.ForeColor = Color.White;
-			CleanCheck.Location = new Point( 12, 72 );
+			CleanCheck.Location = new Point( 10, 54 );
+			CleanCheck.Margin = new Padding( 3, 2, 3, 2 );
 			CleanCheck.Name = "CleanCheck";
-			CleanCheck.Size = new Size( 107, 24 );
+			CleanCheck.Size = new Size( 87, 19 );
 			CleanCheck.TabIndex = 3;
 			CleanCheck.Text = "Clean Install";
 			CleanCheck.UseVisualStyleBackColor = true;
@@ -94,39 +97,42 @@
 			UpdateCheck.AutoSize = true;
 			UpdateCheck.FlatStyle = FlatStyle.Flat;
 			UpdateCheck.ForeColor = Color.White;
-			UpdateCheck.Location = new Point( 12, 12 );
+			UpdateCheck.Location = new Point( 10, 9 );
+			UpdateCheck.Margin = new Padding( 3, 2, 3, 2 );
 			UpdateCheck.Name = "UpdateCheck";
-			UpdateCheck.Size = new Size( 142, 24 );
+			UpdateCheck.Size = new Size( 113, 19 );
 			UpdateCheck.TabIndex = 4;
 			UpdateCheck.Text = "Update SatDump";
 			UpdateCheck.UseVisualStyleBackColor = true;
 			// 
 			// PathButton
 			// 
-			PathButton.BackColor = Color.FromArgb(         68  ,         68  ,         68   );
+			PathButton.BackColor = Color.FromArgb(       68,       68,       68 );
 			PathButton.FlatAppearance.BorderSize = 0;
 			PathButton.FlatStyle = FlatStyle.Flat;
 			PathButton.ForeColor = Color.White;
-			PathButton.Location = new Point( 204, 40 );
+			PathButton.Location = new Point( 178, 30 );
+			PathButton.Margin = new Padding( 3, 2, 3, 2 );
 			PathButton.Name = "PathButton";
-			PathButton.Size = new Size( 127, 29 );
+			PathButton.Size = new Size( 111, 22 );
 			PathButton.TabIndex = 5;
 			PathButton.Text = "Browse For Path";
 			PathButton.UseVisualStyleBackColor = false;
-			PathButton.Click += PathButton_Click;
+			PathButton.Click +=  PathButton_Click ;
 			// 
 			// MainForm
 			// 
-			AutoScaleDimensions = new SizeF( 8F, 20F );
+			AutoScaleDimensions = new SizeF( 7F, 15F );
 			AutoScaleMode = AutoScaleMode.Font;
-			BackColor = Color.FromArgb(         17  ,         17  ,         17   );
-			ClientSize = new Size( 343, 182 );
+			BackColor = Color.FromArgb(       17,       17,       17 );
+			ClientSize = new Size( 300, 136 );
 			Controls.Add( PathButton );
 			Controls.Add( UpdateCheck );
 			Controls.Add( CleanCheck );
 			Controls.Add( StatLabel );
 			Controls.Add( NightlyCheck );
 			Controls.Add( LaunchButton );
+			Margin = new Padding( 3, 2, 3, 2 );
 			Name = "MainForm";
 			ShowIcon = false;
 			StartPosition = FormStartPosition.CenterScreen;
